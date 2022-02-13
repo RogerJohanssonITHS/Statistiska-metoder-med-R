eb686442f8cb2830c2fc9911f57e7e1023748899
RogerJohanssonITHS <89128014+RogerJohanssonITHS@users.noreply.github.com>
2022-02-13 21:52:19 +0100

    Empirisk sannolikhetsfördelning. Block 3.

--- formel.r
+++ formel.r
@@ -2,3 +2,11 @@ binomialPatient <- function(n, x, P) {
    result <- choose(n, x) * P^x * ((1 - P) ^(n - x))
    print(result)
 }
+
+empiricalSamplingDistr <- function(dataset, samples, samplesize) {
+	sampleMeanCollection <- c()
+	for (i in 1:samples){
+		sampleMeanCollection <- c(sampleMeanCollection, mean(sample(dataset, samplesize, replace = T))) # Appending new value to vector
+		}
+	return (sampleMeanCollection)
+}
